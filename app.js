23525d90cd64ab0a03c44c7d4648761de469708a
Chukwuka1488 <juniorakibor@yahoo.com>
2021-05-18 23:40:19 +0200

    custom icon and marker added to darmstadt

--- app.js
+++ app.js
@@ -6,13 +6,26 @@ L.tileLayer(
   {
     attribution:
       '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>',
+    crossOrigin: true,
   }
 ).addTo(map);
+
+//changing marker icon
+var greenIcon = L.icon({
+  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+  shadowUrl: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+
+  iconSize: [38, 95], // size of the icon
+  shadowSize: [50, 64], // size of the shadow
+  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+  shadowAnchor: [4, 62], // the same for the shadow
+  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+});
 
 // adding markers [-ve for west and positive for east]
 var markerLondon = L.marker([51.5, -0.09]).addTo(map);
 var markerLagos = L.marker([6.5, 3.4]).addTo(map);
-var markerDarmstadt = L.marker([49.9, 8.7]).addTo(map);
+var markerDarmstadt = L.marker([49.9, 8.7], { icon: greenIcon }).addTo(map);
 
 //adding circle
 var circleLondon = L.circle([51.508, -0.11], {
@@ -39,3 +52,4 @@ var polygonLagos = L.polygon(
 // adding pop-ups
 markerLondon.bindPopup("<b> Hey There!</b><br>This is London.").openPopup();
 polygonLagos.bindPopup("This is Lagos!!!");
+markerDarmstadt.bindPopup("Welcome to Darmstadt");
