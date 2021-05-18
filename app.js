eac82c6b3434ae4e84c6cbf543e90538a55e1079
Chukwuka1488 <juniorakibor@yahoo.com>
2021-05-18 23:47:16 +0200

    adding multiple icons using a class

--- app.js
+++ app.js
@@ -11,20 +11,42 @@ L.tileLayer(
 ).addTo(map);
 
 //changing marker icon
-var greenIcon = L.icon({
-  iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png",
-  shadowUrl: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+// var greenIcon = L.icon({
+//   iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+//   shadowUrl: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
 
-  iconSize: [38, 95], // size of the icon
-  shadowSize: [50, 64], // size of the shadow
-  iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-  shadowAnchor: [4, 62], // the same for the shadow
-  popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+//   iconSize: [38, 95], // size of the icon
+//   shadowSize: [50, 64], // size of the shadow
+//   iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
+//   shadowAnchor: [4, 62], // the same for the shadow
+//   popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
+// });
+
+// adding multiple icons at once using classes
+var LeafletIcon = L.Icon.extend({
+  options: {
+    shadowUrl: "https://leafletjs.com/examples/custom-icons/leaf-shadow.png",
+    iconSize: [38, 95],
+    shadowSize: [50, 64],
+    iconAnchor: [22, 94],
+    shadowAnchor: [4, 62],
+    popupAnchor: [-3, -76],
+  },
 });
 
+var greenIcon = new LeafletIcon({
+    iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-green.png",
+  }),
+  redIcon = new LeafletIcon({
+    iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-red.png",
+  }),
+  orangeIcon = new LeafletIcon({
+    iconUrl: "https://leafletjs.com/examples/custom-icons/leaf-orange.png",
+  });
+
 // adding markers [-ve for west and positive for east]
-var markerLondon = L.marker([51.5, -0.09]).addTo(map);
-var markerLagos = L.marker([6.5, 3.4]).addTo(map);
+var markerLondon = L.marker([51.5, -0.09], { icon: redIcon }).addTo(map);
+var markerLagos = L.marker([6.5, 3.4], { icon: orangeIcon }).addTo(map);
 var markerDarmstadt = L.marker([49.9, 8.7], { icon: greenIcon }).addTo(map);
 
 //adding circle
